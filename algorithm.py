a593f6d1a334b26a4985262e4a8d084d67930444
alexzcheong <113654786+alexzcheong@users.noreply.github.com>
2024-03-10 14:13:18 -0700

    Update algorithm.py
    some stuff that I dunno how it works lmao

--- algorithm.py
+++ algorithm.py
@@ -16,9 +16,19 @@ class WTFisThis(db.Model):
   date_created = db.Column(db.DateTime(default.datetime.utcnow))
   def __repr__ (self):
     return '<Event %r>' % self.id
-@app.route("/")
-#https://www.programiz.com/python-programming/time <-time lib documentation we porob need later
 
+
+#set a form+action on our frontend with the corresponding methods for event creation
+#prob gonna need more methods for group creation, friend adding, all that fun stuff
+@app.route("/", methods=['ADD_EVENT', 'DELETE_EVENT'])
+#https://www.programiz.com/python-programming/time <-time lib documentation we porob need later
+def generateEvent(dateTime):
+  if request.method=='ADD_EVENT':
+    pass #todo add calendar event
+  else:
+    pass #todo remove calendar event if present
+
+#calendar_list should be a list of tuples with start and end times [ {s1, e1}, {s2, e2} , etc]
 def generateFreeTimes(calender_list):
     #do things, no idea how data is gonna be passed but we'll see
   return render.template('/')#url redirect I think, or a render display go figure
