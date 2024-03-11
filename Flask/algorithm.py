9b24bab0dc19664c266955374c88caf6228c2491
alexzcheong <113654786+alexzcheong@users.noreply.github.com>
2024-03-10 20:31:03 -0700

    Update algorithm.py
    minor changes

--- Flask/algorithm.py
+++ Flask/algorithm.py
@@ -21,16 +21,27 @@ class WTFisThis(db.Model):
 #set a form+action on our frontend with the corresponding methods for event creation
 #prob gonna need more methods for group creation, friend adding, all that fun stuff
 @app.route("/", methods=['ADD_EVENT', 'DELETE_EVENT'])
-#https://www.programiz.com/python-programming/time <-time lib documentation we porob need later
-def generateEvent(dateTime):
+
+#RESOURCES
+#https://www.programiz.com/python-programming/time <-time lib documentation we porob need later\
+#https://www.programiz.com/python-programming/datetime/strptime <-also very useful
+def generateEvent(startTime, endTime):
   if request.method=='ADD_EVENT':
-    pass #todo add calendar event
+    dt_start = datetime.strptime(startTime, "%d/%m/%Y %H:%M:%S")#start time?
+    dt_end = datetime.strptime(endTime, "%d/%m/%Y %H:%M:%S")
+    return
   else:
-    pass #todo remove calendar event if present
+    #check for event in db and delete, how tf do I find an event???
+    return
 
 #calendar_list should be a list of tuples with start and end times [ {s1, e1}, {s2, e2} , etc]
 def generateFreeTimes(calender_list):
-    #do things, no idea how data is gonna be passed but we'll see
+  for timeTuple in calendar_list:
+    #wtf format am I using here? strings? unix? I need to know
+    start = timeTuple[0]
+    end = timeTuple[1]
+    #do math??? check to see if we cant just display occupied times instead
+  
   return render.template('/')#url redirect I think, or a render display go figure
 
 if __name__ == "__main__":
