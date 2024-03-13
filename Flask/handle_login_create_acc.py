f7d09cb4e6296e4de11b847e0c070397802099c8
HemanthKarnati <87663677+HemanthKarnati@users.noreply.github.com>
2024-03-13 15:21:37 -0700

    Update handle_login_create_acc.py

--- Flask/handle_login_create_acc.py
+++ Flask/handle_login_create_acc.py
@@ -20,12 +20,14 @@ def handle_create_account():
         username = request.form['username']
         email = request.form['email_address']
         password = request.form['password']
+        first = request.form['first_name']
+        last = request.form['last_name']
 
         userObj = flask_sqlalchemy_db_setup.Users.query.get(username)
 
         if userObj is None:
-            new_item = User(None,username,password)
-            flask_sqlalchemy_db_setup.db.insert(userObj)
+            new_item = User(None,first,last,username,password)
+            flask_sqlalchemy_db_setup.db.insert(new_item)
             flask_sqlalchemy_db_setup.db.session.commit()
             return True
         else:
