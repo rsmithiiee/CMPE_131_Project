5f12054f16658dfcedbe5da24b1d885a42e7e75a
HemanthKarnati <hemanthkarnati101@gmail.com>
2024-03-13 14:56:30 -0700

    Update handle_login_create_acc.py

--- Flask/handle_login_create_acc.py
+++ Flask/handle_login_create_acc.py
@@ -1,20 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, Blueprint
 import flask_sqlalchemy_db_setup
 
-app = Flask(__name__)
-@app.route('/handle_login', method = ['GET','POST'])
+login_bp = Blueprint('login_bp', __name__)
+@login_bp.route('/handle_login', method = ['GET','POST'])
 def handle_login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         userObj = flask_sqlalchemy_db_setup.Users.query.get(username)
         if userObj.Password == password and userObj is not None:
-            flask_sqlalchemy_db_setup.db.add_user(userObj)
-            flask_sqlalchemy_db_setup.db.session.commit()
+            # flask_sqlalchemy_db_setup.db.add_user(userObj)
+            # flask_sqlalchemy_db_setup.db.session.commit()
             return True
         else:
             return False
-@app.route('/handle_create_account', method = ['POST'])
+@login_bp.route('/handle_create_account', method = ['POST'])
 def handle_create_account():
     if request.method == 'POST':
         username = request.form['username']
@@ -24,6 +24,8 @@ def handle_create_account():
         userObj = flask_sqlalchemy_db_setup.Users.query.get(username)
 
         if userObj is None:
+            flask_sqlalchemy_db_setup.db.add_user(userObj)
+            flask_sqlalchemy_db_setup.db.session.commit()
             return True
         else:
             return False
