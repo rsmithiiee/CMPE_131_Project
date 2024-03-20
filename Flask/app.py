fac5bd8b0144ab26fe34b04aca693bd68693a0be
HemanthKarnati <hemanthkarnati101@gmail.com>
2024-03-20 16:17:10 -0700

    pushed unfinished functionality

--- Flask/app.py
+++ Flask/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy_db_setup import db, Users
+from flask_sqlalchemy_db_setup import db, Users, Groups
 # from flask_cors import CORS
 import sqlite3
 from sqlalchemy import select
-from argon2 import PasswordHasher
+#from argon2 import PasswordHasher
 
 #initialize flask instance
 app = Flask(__name__)
@@ -36,12 +36,31 @@ def handle_create_account():
 
         stmt = db.session.scalars(select(Users).where(Users.Username == username)).first()
         if stmt is None:
-            user = Users(First_Name = first_name, Last_Name = last_name, Username = username, Password = hashed_password)
+            user = Users(First_Name = first_name, Last_Name = last_name, Username = username, Password = password)
             db.session.add(user)
             db.session.commit()
         else:
             return jsonify({'success': False, 'message': 'Username taken'})
-        
+
+@app.route('api/create_group', methods = ['GET','POST'])
+def create_group():
+    if request.method == 'POST':
+        ID = request.form['user_id']
+        group_name = request.form['group_name']
+        group = Groups(Group_Name = group_name)
+        db.session.add(group)
+        db.session.commit()
+        #TODO: finish code
+
+    return jsonify({'success': True, 'message': 'Group Created!'})
+
+@app.route('api/add_users_group', methods = ['GET','POST'])
+def addToGroup():
+    if request.method == 'POST':
+        name = request.form['username']
+        #TODO: Finish code
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
