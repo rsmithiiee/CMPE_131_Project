37cecfb378cd252352799047dad0d067feb362ff
rsmithiiee <137942077+rsmithiiee@users.noreply.github.com>
2024-03-20 15:46:36 -0700

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,25 +1,47 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy_db_setup import db, Users
-from handle_login_create_acc import login_bp
-from flask_cors import CORS
+# from flask_cors import CORS
+import sqlite3
 from sqlalchemy import select
+from argon2 import PasswordHasher
 
+#initialize flask instance
 app = Flask(__name__)
+
+#initialize and create database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///official.db"
 db.init_app(app)
-
 with app.app_context():
     db.create_all()
 
-app.register_blueprint(login_bp)
+#Login and create account routes
+@app.route('/login', methods = ['GET', 'POST'])
+def handle_login():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            stmt = db.session.scalars(select(Users).where(Users.Username == username).where(Users.Password == password)).first()
+        if stmt is None:
+            return jsonify({'success': False, 'message': 'Login failed'})    
+        else:
+            return jsonify({'success': True, 'message': 'Login successful'})
+    
+@app.route('/create_account', methods = ['GET','POST'])
+def handle_create_account():
+    if request.method == 'POST':
+        username = request.form['username']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
 
-@app.route('/test')
-def check_login():
-    username = 'test'
-    password = 'test'
-    result = db.session.execute(select(Users).filter_by(Users.User_ID)).first()       
-
-    result.all()
+        stmt = db.session.scalars(select(Users).where(Users.Username == username)).first()
+        if stmt is None:
+            user = Users(First_Name = first_name, Last_Name = last_name, Username = username, Password = hashed_password)
+            db.session.add(user)
+            db.session.commit()
+        else:
+            return jsonify({'success': False, 'message': 'Could not create account'})
+        
 
 if __name__ == "__main__":
     app.run(debug = True)
