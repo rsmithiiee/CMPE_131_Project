ded655aba20c793615eea7531c40dee33e0577b6
HemanthKarnati <hemanthkarnati101@gmail.com>
2024-04-20 10:43:05 -0700

    changed test file

--- Flask/app.py
+++ Flask/app.py
@@ -3,72 +3,91 @@ from flask_sqlalchemy_db_setup import db, Users, Groups, group_users_m2m
 # from flask_cors import CORS
 import sqlite3
 from sqlalchemy import select
-#from argon2 import PasswordHasher
 
-#initialize flask instance
+# from argon2 import PasswordHasher
+
+# initialize flask instance
 app = Flask(__name__)
 
-#initialize and create database
+# initialize and create database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///official.db"
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
-#Login and create account routes
-@app.route('/login', methods = ['GET', 'POST'])
+
+# Login and create account routes
+@app.route('/api/login', methods=['GET', 'POST'])
 def handle_login():
         if request.method == 'POST':
-            data = request.json
-            username = data.get('username')
-            password = data.get('password')
-
-            ph = PasswordHasher()
-            hashed_password = ph.hash(password)
-            stmt = db.session.scalars(select(Users).where(Users.Username == username).where(Users.Password == hashed_password)).first()
-            check_pass = ph.verify(stmt.Password, password)
-
-        if stmt is None or check_pass != True:
-            return jsonify({'success': False})    
-        else:
-            return jsonify({'success': True})
+            if request.method == 'POST':
+                data = request.get_json()
+                username = data['username']
+                password = data['password']
+                stmt = db.session.scalars(
+                    select(Users).where(Users.Username == username).where(Users.Password == password)).first()
+                # ph = PasswordHasher
+                # check_pass = ph.verify(stmt.Password, password)
+                # or check_pass != True
+            if stmt is None:
+                return jsonify({'success': False, 'message': 'Login failed'})
+            else:
+                return jsonify({'success': True, 'message': 'Login successful'})
     
-@app.route('/create_account', methods = ['GET','POST'])
+# @app.route('/create_account', methods = ['GET','POST'])
+# def handle_create_account():
+#     if request.method == 'POST':
+#         data = request.json()
+#         username = data.get('username')
+#         first_name = data.get('first_name')
+#         last_name = data.get('last_name')
+#         password = data.get('password')
+#
+#         stmt = db.session.scalars(select(Users).where(Users.Username == username)).first()
+#         if stmt is None:
+#             # ph = PasswordHasher()
+#             # hashed_password = ph.hash()
+#             user = Users(First_Name = first_name, Last_Name = last_name, Username = username, Password = password)
+
+
+@app.route('/api/create_account', methods=['GET', 'POST'])
 def handle_create_account():
     if request.method == 'POST':
-        data = request.json()
-        username = data.get('username')
-        first_name = data.get('first_name')
-        last_name = data.get('last_name')
-        password = data.get('password')
-        
+        data = request.get_json()
+        first_name = data['first_name']
+        last_name = data['last_name']
+        username = data['username']
+        password = data['password']
+        # ph = PasswordHasher()
+        # hashed_password = ph.hash()
+
         stmt = db.session.scalars(select(Users).where(Users.Username == username)).first()
         if stmt is None:
-            ph = PasswordHasher()
-            hashed_password = ph.hash()
-            user = Users(First_Name = first_name, Last_Name = last_name, Username = username, Password = hashed_password)
+            user = Users(First_Name=first_name, Last_Name=last_name, Username=username, Password=password)
             db.session.add(user)
             db.session.commit()
             return jsonify({'success': False})
         else:
             return jsonify({'success': True})
 
-@app.route('api/create_group', methods = ['GET','POST'])
+
+@app.route('/api/create_group', methods=['GET', 'POST'])
 def create_group():
     if request.method == 'POST':
         data = request.json
         ID = data.get('user_id')
         group_name = data.get('group_name')
-        group = Groups(Group_Name = group_name)
-        #TODO: Get group ID after it has been added to group Table
+        group = Groups(Group_Name=group_name)
         db.session.add(group)
         group_id = db.session.execute("SELECT last_insert_rowid()")
-        input = group_users_m2m(User_ID = ID, Group_ID = group_id)
+        input = group_users_m2m(User_ID=ID, Group_ID=group_id)
         db.session.add(input)
-        #db.session.execute("INSERT INTO Group_Users (User_ID, Group_ID) VALUES (?,?)", (ID, group_ID))
+        # db.session.execute("INSERT INTO Group_Users (User_ID, Group_ID) VALUES (?,?)", (ID, group_ID))
         db.session.commit()
     return jsonify({'success': True})
 
-@app.route('api/add_users_group', methods = ['GET','POST'])
+
+@app.route('/api/add_users_group', methods=['GET', 'POST'])
 def addToGroup():
     if request.method == 'POST':
         data = request.json
@@ -78,7 +97,7 @@ def addToGroup():
         if user or group is None:
             return jsonify({'success': False, 'message': 'User/Group not found!'})
         else:
-            #TODO: How to retrieve group ID from DB
+            # TODO: How to retrieve group ID from DB
             user_id = user.USER_ID
             group_id = group.GROUP_ID
             group_entry = group_users_m2m.insert().values(User_ID=user_id, Group_ID=group_id)
@@ -86,10 +105,11 @@ def addToGroup():
             db.session.commit()
     return jsonify({'success': True, 'message': 'Group Info Updated'})
 
-@app.route('api/delete_user_group', methods = ['POST'])
+
+@app.route('/api/delete_user_group', methods=['POST'])
 def removeFromGroup():
     return jsonify({'success': True, 'message': 'User successfully deleted!'})
 
-if __name__ == "__main__":
-    app.run(debug = True)
 
+if __name__ == "__main__":
+    app.run(debug=True)
