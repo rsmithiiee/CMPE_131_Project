9794a37285bf1b0d72affc0736300cb5c33c5e42
rsmithiiee <137942077+rsmithiiee@users.noreply.github.com>
2024-04-23 17:58:14 -0700

    fixed many to many relationship between groups and users

--- Flask/flask_sqlalchemy_db_setup.py
+++ Flask/flask_sqlalchemy_db_setup.py
@@ -1,24 +1,29 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-import sqlalchemy as sa
+from sqlalchemy import ForeignKey, Column, Table
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import List
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///official.db"
 
 class Base(DeclarativeBase):
     pass
 
-db = SQLAlchemy(app, model_class = Base)
+db = SQLAlchemy(model_class = Base)
+
+Group_Users_m2m = Table(
+    "Groups_Users",
+    Base.metadata,
+    Column("Groups", ForeignKey("Groups.Group_ID"), primary_key = True),
+    Column("Users", ForeignKey("Users.User_ID"), primary_key = True),
+)
 
 class Users(db.Model):
     __tablename__ = "Users"
     User_ID : Mapped[int] = mapped_column(primary_key = True)
     First_Name : Mapped[str]
     Last_Name : Mapped[str]
-    Username : Mapped[str] = mapped_column(unique = True, nullable = False)
+    Username : Mapped[str] = mapped_column(unique = True)
     Password : Mapped[str]
+    User_Groups : Mapped[List["Groups"]] = relationship(secondary = Group_Users_m2m, back_populates = "Group_Users")
 
 class User_Events(db.Model):
     __tablename__ = "User_Events"
@@ -32,19 +37,5 @@ class Groups(db.Model):
     __tablename__ = "Groups"
     Group_ID : Mapped[int] = mapped_column(primary_key = True)
     Group_Name : Mapped[str]
+    Group_Users : Mapped[List["Users"]] = relationship(secondary = Group_Users_m2m, back_populates = "User_Groups")
 
-class Group_Events(db.Model):
-    __tablename__ = "Group Events"
-    Event_ID : Mapped[int] = mapped_column(primary_key = True)
-    Group_ID : Mapped[int]
-    Start_Time : Mapped[str]
-    End_Time : Mapped[str]
-
-group_users_m2m = db.Table(
-    "Group_Users",
-    sa.Column("User_ID", sa.ForeignKey(Users.User_ID), primary_key = True),
-    sa.Column("Group_ID", sa.ForeignKey(Groups.Group_ID), primary_key = True),
-)
-
-with app.app_context():
-    db.create_all()
