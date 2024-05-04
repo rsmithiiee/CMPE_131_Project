495ece335ad5f6b0038d87f7305cb3e9e03b89dd
rsmithiiee <137942077+rsmithiiee@users.noreply.github.com>
2024-05-04 15:06:46 -0700

    added group free times and retrieve user events endpoint

--- Flask/app.py
+++ Flask/app.py
@@ -28,21 +28,21 @@ def handle_login():
             username = data.get('username')
             password = data.get('password')
 
-        ph = PasswordHasher()
-        stmt = db.session.scalars(select(Users).where(Users.Username == username)).first()
-        if stmt is None:
-            return jsonify({'success' : False})
-        
-        check_pass = False
-        try:
-            check_pass = ph.verify(stmt.Password, password)
-        except: 
-            pass
+            ph = PasswordHasher()
+            stmt = db.session.scalars(select(Users).where(Users.Username == username)).first()
+            if stmt is None:
+                return jsonify({'success' : False})
             
-        if check_pass != True:
-            return jsonify({'success' : False})    
-        else:
-            return jsonify({'success': True})
+            check_pass = False
+            try:
+                check_pass = ph.verify(stmt.Password, password)
+            except: 
+                pass
+                
+            if check_pass != True:
+                return jsonify({'success' : False})    
+            else:
+                return jsonify({'success': True})
     
 @app.route('/api/create_account', methods = ['GET','POST'])
 def handle_create_account():
@@ -119,6 +119,71 @@ def delete_event():
         else:
             db.session.commit()
             return jsonify({'success' : True})
+
+#group free times and retrieve user events endpoints
+@app.route("/api/retrieve_group_free_times", methods = ["GET", "POST"])
+def retrieve_group_free_times():
+    if request.method == "POST":
+        data = request.json
+        start_time = data.get("start_time")
+        end_time = data.get("end_time")
+        group_id = data.get("group_id")
+
+        #list of tuples to store user events
+        user_events = []
+        #add tuple of start time to list
+        start_time_tuple = (start_time, start_time)
+        user_events.append(start_time_tuple)
+        #search for group with group id
+        rc = db.session.scalars(select(Groups).where(Groups.Group_ID == group_id)).first()
+        
+        #if no group is found return false
+        if rc is None:
+            return jsonify({"success":False})
+        #iterate through each user in the group
+        for Users in rc.Group_Users:
+            #iterate through the events of each user and append the event's (start time, end time) as a tuple to the user events list
+            for User_Events in Users.Events_of_User:
+                events = (User_Events.Start_Time, User_Events.End_Time)
+                user_events.append(events)
+        #append the end time to the list
+        user_events.append((end_time, end_time))
+
+        #pass the list to the algorithm
+        free_times_list = superfn(user_events)
+
+        #return the array of json objects
+        return free_times_list
+    
+@app.route("/api/retrieve_user_events", methods = ["GET", "POST"])
+def retrieve_user_events():
+    if request.method == "POST":
+        data = request.json
+        start_time = data.get("start_time")
+        end_time = data.get("end_time")
+        user_id = data.get("user_id")
+
+        #list to store json objects for each events
+        user_events = []
+        
+        #find user with matching user id
+        rc = db.session.scalars(select(Users).where(Users.User_ID == user_id)).first()
+
+        #if no user found return false
+        if rc is None:
+            return jsonify({"success":False})
+        
+        #iterate through the events of user list and for each event create a json object and append to the list
+        for User_Events in rc.Events_of_User:
+            events_json = {"Title" : User_Events.Event_Name, "start" : User_Events.Start_Time, "end" : User_Events.End_Time, "allDay" : False, "id" : User_Events.Event_ID}
+            user_events.append(events_json)
+
+        #return false if no events
+        if not user_events:
+            return jsonify({"success":False})
+        
+        #return the array of json objects
+        return jsonify(user_events)
 
 #group endpoints
 @app.route('/api/create_group', methods=['GET', 'POST'])
